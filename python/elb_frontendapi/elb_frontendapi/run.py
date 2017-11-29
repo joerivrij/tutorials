3f9ba12e65ac456a0f6f935e86ccdfa459db7e70
joerivrij <joeri@beveragerandomizer.com>
2017-11-29 20:41:21 +0100

    First commit

---
+++ python/elb_frontendapi/elb_frontendapi/run.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3010)
